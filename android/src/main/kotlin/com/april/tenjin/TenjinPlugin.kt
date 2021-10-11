b009b122b44a5b2494182dacc6776f00e5004a59
wq <wq@qq.com>
2021-10-11 17:36:12 +0800

    'Flutter端的代码待实现'

--- android/src/main/kotlin/com/april/tenjin/TenjinPlugin.kt
+++ android/src/main/kotlin/com/april/tenjin/TenjinPlugin.kt
@@ -1,6 +1,8 @@
 package com.april.tenjin
 
+import android.content.Context
 import androidx.annotation.NonNull
+import com.tenjin.android.TenjinSDK
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -9,23 +11,117 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** TenjinPlugin */
-class TenjinPlugin: FlutterPlugin, MethodCallHandler {
-  private lateinit var channel : MethodChannel
+class TenjinPlugin : FlutterPlugin, MethodCallHandler {
+    private lateinit var channel: MethodChannel
+    private lateinit var context: Context
+    private var sdk: TenjinSDK? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "tenjin")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "april_tenjin")
+        context = flutterPluginBinding.applicationContext
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method != "init") {
+            if (sdk == null) {
+                result.error("需要先调用 init 初始化 TenjinSDK", null, null)
+                return
+            }
+        }
+        when (call.method) {
+            "init" -> {
+                val apiKey = call.argument("apiKey") as? String
+                if (apiKey.isNullOrEmpty()) {
+                    result.success(false)
+                } else {
+                    sdk = TenjinSDK.getInstance(context, apiKey)
+                    result.success(true)
+                }
+            }
+            "optIn" -> {
+                sdk!!.optIn()
+                result.success(true)
+            }
+            "optOut" -> {
+                sdk!!.optOut()
+                result.success(true)
+            }
+            "optInParams" -> {
+                sdk!!.optInParams(
+                    (call.argument("params") as? List<String>)?.toTypedArray()
+                        ?: emptyArray<String>()
+                )
+                result.success(true)
+            }
+            "optOutParams" -> {
+                sdk!!.optOutParams(
+                    (call.argument("params") as? List<String>)?.toTypedArray()
+                        ?: emptyArray<String>()
+                )
+                result.success(true)
+            }
+            "connect" -> {
+                sdk!!.connect()
+                result.success(true)
+            }
+            "transaction" -> {
+                sdk!!.transaction(
+                    (call.argument("productId") as? String) ?: "",
+                    (call.argument("currencyCode") as? String) ?: "",
+                    (call.argument("quantity") as? Int) ?: 0,
+                    (call.argument("unitPrice") as? Double) ?: 0.0,
+                    (call.argument("purchaseData") as? String) ?: "",
+                    (call.argument("dataSignature") as? String ?: "")
+                )
+                result.success(true)
+            }
+            "eventWithName" -> {
+                val name = call.argument("name") as? String
+                if (name.isNullOrEmpty()) {
+                    result.success(false)
+                } else {
+                    sdk!!.eventWithName(name)
+                    result.success(true)
+                }
+            }
+            "eventWithNameAndIntValue" -> {
+                val name = call.argument("name") as? String
+                val value = call.argument("value") as? Int
+                if (name.isNullOrEmpty() || value == null) {
+                    result.success(false)
+                } else {
+                    sdk!!.eventWithNameAndValue(name, value)
+                    result.success(true)
+                }
+            }
+            "eventWithNameAndStringValue" -> {
+                val name = call.argument("name") as? String
+                val value = call.argument("value") as? String
+                if (name.isNullOrEmpty() || value == null) {
+                    result.success(false)
+                } else {
+                    sdk!!.eventWithNameAndValue(name, value)
+                    result.success(true)
+                }
+            }
+            "appendAppSubversion" -> {
+                val value = call.argument("value") as? Int
+                if (value == null) {
+                    result.success(false)
+                } else {
+                    sdk!!.appendAppSubversion(value)
+                    result.success(true)
+                }
+            }
+            else -> {
+                result.notImplemented()
+            }
+        }
+    }
+
 }
--- lib/tenjin.dart
+++ lib/tenjin.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Tenjin {
-  static const MethodChannel _channel = MethodChannel('tenjin');
+  static const MethodChannel _channel = MethodChannel('april_tenjin');
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
